b19ef337bef0b7facf5321b4f294c27a2581196e
rzadrbi <rzadrbi@gmail.com>
2024-04-23 18:39:11 +0330

    fix the logout 2

--- reservation/views.py
+++ reservation/views.py
@@ -337,7 +337,7 @@ def verify_payment_adineh(request):
 
 def signout(request):
     user = User.objects.get(username=request.user.username)
-    user.delete()
     logout(request)
+    user.delete()
     return redirect('reservation:index')
 
